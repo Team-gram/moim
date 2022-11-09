844d8902c0ef07a4b870ccb180dcc037e476b0a1
baegyu <baelanche@naver.com>
2022-11-09 12:55:32 +0900

    add Moim Select, Insert

---
+++ src/main/java/ajou/gram/moim/controller/MoimController.java
@@ -0,0 +1,36 @@
+package ajou.gram.moim.controller;
+
+import ajou.gram.moim.domain.Moim;
+import ajou.gram.moim.service.MoimService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/moim")
+public class MoimController {
+
+    private MoimService moimService;
+
+    @Autowired
+    public MoimController(MoimService moimService) {
+        this.moimService = moimService;
+    }
+
+    @GetMapping("/cate/{category_id}")
+    public List<Moim> getMoims(@PathVariable("category_id") int categoryId) {
+        return moimService.getMoims(categoryId);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Moim> getMoim(@PathVariable("id") long id) {
+        return moimService.getMoim(id);
+    }
+
+    @PostMapping({" ", "/"})
+    public void addMoim(@RequestBody Moim moim) {
+        moimService.addMoim(moim);
+    }
+}
---
+++ src/main/java/ajou/gram/moim/repository/MoimRepository.java
@@ -0,0 +1,10 @@
+package ajou.gram.moim.repository;
+
+import ajou.gram.moim.domain.Moim;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MoimRepository extends JpaRepository<Moim, Long> {
+    List<Moim> findByCategoryId(int categoryId);
+}
