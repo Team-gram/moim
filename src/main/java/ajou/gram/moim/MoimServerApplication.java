127a10554f240901bcc6dba4c8be3c465998c3aa
baegyu <baelanche@naver.com>
2022-11-29 16:39:07 +0900

    Update: Divide yml

--- src/main/java/ajou/gram/moim/MoimServerApplication.java
+++ src/main/java/ajou/gram/moim/MoimServerApplication.java
@@ -2,11 +2,17 @@ package ajou.gram.moim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MoimServerApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
 	public static void main(String[] args) {
-		SpringApplication.run(MoimServerApplication.class, args);
+		new SpringApplicationBuilder(MoimServerApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 }
