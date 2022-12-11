dcbeb8fc812df92be6c3f7c0355bef59c1cfd6cf
baegyu <baelanche@naver.com>
2022-12-12 01:51:42 +0900

    BugFix: POST /meet/upper

--- src/main/java/ajou/gram/moim/controller/MeetController.java
+++ src/main/java/ajou/gram/moim/controller/MeetController.java
@@ -18,6 +18,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
@@ -162,7 +163,7 @@ public class MeetController {
             @ApiResponse(responseCode = "200", description = "상위노출 업소 등록 성공"),
             @ApiResponse(responseCode = "400", description = "상위노출 업소 등록 실패")
     })
-    @PostMapping("/upper")
+    @PostMapping(value = "/upper", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<?> addUpperPlace(@ModelAttribute MoimPlaceUpperDto moimPlaceUpperDto) {
         try {
             moimUpperService.addUpperPlace(moimPlaceUpperDto);
